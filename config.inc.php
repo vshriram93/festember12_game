2157795781951cbc6f4f08f93cc83ee1a5ad7d54
steve <vshriram93@gmail.com>
2012-09-12 00:26:58 +0530

    basic auth completed

--- config.inc.php
+++ config.inc.php
@@ -1,11 +1,14 @@
 <?php
 define("MYSQL_SERVER","localhost");                                  
-define("MYSQL_USERNAME","");
-define("MYSQL_PASSWORD","");
-define("MYSQL_DATABASE","");
+define("MYSQL_USERNAME","festember12_game");
+define("MYSQL_PASSWORD","GetLucky");
+define("MYSQL_DATABASE","festember12_games");
+define("APP_ID","263593520331172");
+define("APP_SECRET","f375faf3a72816923278d367798ce2aa");
+
               
 $connect=mysql_connect(MYSQL_SERVER,MYSQL_USERNAME,MYSQL_PASSWORD);
-if(!$connect) echo "could not connect to server";
-$dbase=mysql_select_db(MYSQL_DATABSE,$connect);
-if(!$dbase) echo "could not connect to database";
+if(!$connect) {echo "could not connect to server";exit();}
+$dbase=mysql_select_db(MYSQL_DATABASE,$connect);
+if(!$dbase) {echo "could not connect to database";exit();}
 ?>
--- index.php
+++ index.php
@@ -1,42 +1,46 @@
 <?php
 @include_once("config.inc.php");
 
-$appid = "";                                                                                                              
-$appsecret = '';                                                                                         
 $canvas_page = "";                                                                                    
 require 'fb/facebook.php';                                                                                                               
 $facebook = new Facebook(array(                                                                                                          
-			       'appId'  => '',                                                                             
-			       'secret' => '', 																 ));                                                                                                        
-
-
-
+			       'appId'  => APP_ID,                                                                             
+			       'secret' => APP_SECRET, 									
+	     	));
 
 
 function loginFB() {
+  global $facebook;
+  print_r($facebook);
   if(!$facebook->getUser()) header("Location:".$facebook->getLoginUrl(array(
 									    'scope' => 'publish_stream'
                                                                             )));
+  exit();
+
 }
 
 function postOnWallWithoutAccess($link,$message) {
-  
+  global $facebook;
+ 
   try {                                                                                                                                   
-    $ret_obj = $facebook->api('/me/feed', 'POST',                                                                                            			      array(                                                                                                         				    'link' => $link,                                                               
-				 'message' => $message                                                                             
-				 ));
-  } catch(FacebookApiException $e) {                                                                                                          $login_url = $facebook->getLoginUrl( array(                                                                                                             'scope' => 'publish_stream'                                                                                         
-			      ));                                                                                           
-    echo 'Please <a href="' . $login_url . '">login.</a>';                                                                                    error_log($e->getType());                                                                                                                 error_log($e->getMessage());                                                                                                                                                                                                                           
-  }                                                                                                                                        
-
-
-
-
+    $ret_obj = $facebook->api('/me/feed', 'POST',   
+			      array(
+				    'link' => $link,
+				    'message' => $message                                                                             
+				    ));
+  } catch(FacebookApiException $e) 
+      { 
+	$login_url = $facebook->getLoginUrl(array('scope' => 'publish_stream' )); 
+	echo 'Please <a href="' . $login_url . '">login.</a>';  error_log($e->getType()); error_log($e->getMessage());
+      }
 }
 
 function init() {
-  if(!$facebook->getUser()) loginFB();    
+  print_r($facebook);
+  global $facebook;
+  if(!$facebook->getUser()) loginFB();
+  postOnWallWithoutAccess("delta.nitt.edu","festi game");
   
-
-} 
+  header("Location: http://delta.nitt.edu");
+}
+init();
