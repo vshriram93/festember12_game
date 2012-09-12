0dc503844ec5442b530f3572a2ff9505919078a4
shriram <vshriram93@gmail.com>
2012-09-12 13:40:00 +0530

    config file added

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+define("MYSQL_SERVER","");                                  
+define("MYSQL_USERNAME","");
+define("MYSQL_PASSWORD","");
+define("MYSQL_DATABASE","");
+define("APP_ID","");
+define("APP_SECRET","");
+
+              
+$connect=mysql_connect(MYSQL_SERVER,MYSQL_USERNAME,MYSQL_PASSWORD);
+if(!$connect) {echo "could not connect to server";exit();}
+$dbase=mysql_select_db(MYSQL_DATABASE,$connect);
+if(!$dbase) {echo "could not connect to database";exit();}
+?>
