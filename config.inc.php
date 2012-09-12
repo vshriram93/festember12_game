59352b459c9c08d884798446d9a073e2ecfc9b67
shriram <vshriram93@gmail.com>
2012-09-12 13:37:28 +0530

    config file modified

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
-define("MYSQL_SERVER","localhost");                                  
+define("MYSQL_SERVER","");                                  
 define("MYSQL_USERNAME","");
 define("MYSQL_PASSWORD","");
 define("MYSQL_DATABASE","");
