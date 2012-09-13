c9aa5f9b940a44dee7ea353bdaa6ad64ac12e644
steve <vshriram93@gmail.com>
2012-09-13 22:57:30 +0530

    fb with a game :)

--- config.inc.php
+++ config.inc.php
@@ -1,10 +1,10 @@
 <?php
 define("MYSQL_SERVER","localhost");                                  
-define("MYSQL_USERNAME","festember12_game");
-define("MYSQL_PASSWORD","GetLucky");
-define("MYSQL_DATABASE","festember12_games");
-define("APP_ID","263593520331172");
-define("APP_SECRET","f375faf3a72816923278d367798ce2aa");
+define("MYSQL_USERNAME","");
+define("MYSQL_PASSWORD","");
+define("MYSQL_DATABASE","");
+define("APP_ID","");
+define("APP_SECRET","");
 
 if(!$connect) {echo "could not connect to  server";exit();}
 $dbase=mysql_select_db(MYSQL_DATABASE,$connect);
